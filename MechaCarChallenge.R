778ede04e732abff7e8ab495abd35b3f014d69b5
giseledoan <88763674+giseledoan@users.noreply.github.com>
2021-12-05 18:14:17 -0800

    Update MechaCarChallenge.R

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,1 +1,14 @@
+library(dplyr) #Load dplyr package
 
+MechaCar_mpg <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F) # read in the MechaCar_mpg.csv file
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data= MechaCar_mpg) # linear regression
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data= MechaCar_mpg)) # determine the p-value and the r-squared value
+
+Suspension_Coil <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F) # read in the Suspension_Coil.csv file
+total_summary <- Suspension_Coil %>% summarize(Mean=mean(PSI),Median=(PSI),Variance=var(PSI),SD=sd(PSI)) # get the mean, median, variance, and standard deviation
+lot_summary <-Suspension_Coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=(PSI),Variance=var(PSI),SD=sd(PSI))
+
+t.test(Suspension_Coil$PSI,mu = 1500) #t-test all lots
+t.test(subset(Suspension_Coil,Manufacturing_Lot=="Lot1")$PSI,mu = 1500) #t-test lot 1
+t.test(subset(Suspension_Coil,Manufacturing_Lot=="Lot2")$PSI,mu = 1500) #t-test lot 2
+t.test(subset(Suspension_Coil,Manufacturing_Lot=="Lot3")$PSI,mu = 1500) #t-test lot 3
