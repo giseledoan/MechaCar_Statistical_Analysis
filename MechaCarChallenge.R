37b55fe3d326702a07cf9584bf4558d4fc32aead
giseledoan <88763674+giseledoan@users.noreply.github.com>
2021-12-05 22:15:43 -0800

    Update MechaCarChallenge.R

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -5,9 +5,9 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AW
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data= MechaCar_mpg)) # determine the p-value and the r-squared value
 
 Suspension_Coil <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F) # read in the Suspension_Coil.csv file
-total_summary <- Suspension %>% summarize(Mean_PSI=mean(PSI), Median_PSI=median(PSI), Var_PSI=var(PSI), Std_Dev_PSI=sd(PSI), Num_Coil=n(), .groups = 'keep') # get the mean, median, variance, and standard deviation
-lot_summary <-Suspension_Coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=(PSI),Variance=var(PSI),SD=sd(PSI))
-
+total_summary <- Suspension_Coil %>% summarize(Mean_PSI=mean(PSI), Median_PSI=median(PSI), Var_PSI=var(PSI), Std_Dev_PSI=sd(PSI), Num_Coil=n(), .groups = 'keep') # get the mean, median, variance, and standard deviation
+lot_summary <- Suspension_Coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean_PSI=mean(PSI), Median_PSI=median(PSI),  Var_PSI=var(PSI), Std_Dev_PSI=sd(PSI), Num_Coil=n(), .groups = 'keep')   
+                                              
 t.test(Suspension_Coil$PSI,mu = 1500) #t-test all lots
 t.test(subset(Suspension_Coil,Manufacturing_Lot=="Lot1")$PSI,mu = 1500) #t-test lot 1
 t.test(subset(Suspension_Coil,Manufacturing_Lot=="Lot2")$PSI,mu = 1500) #t-test lot 2
